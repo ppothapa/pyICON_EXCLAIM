ebeaaec3871beaeb26df0d004367087027565103
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2019-12-16 21:30:15 +0100

    config_ckdtree.py: added 0.05deg grid.

--- config_ckdtree.py
+++ config_ckdtree.py
@@ -15,6 +15,7 @@ all_grids = [
   'global_1.0',
   'global_0.3',
   'global_0.1',
+  'global_0.05',
             ]
 
 all_secs = [
@@ -34,7 +35,7 @@ gnames += ['OCEANINP_pre04_LndnoLak_039km_editSLOHH2017_G']
 # r2b8
 gnames += ['OceanOnly_Global_IcosSymmetric_0010km_rotatedZ37d_modified_srtm30_1min']
 # r2b9
-#gnames += ['OceanOnly_IcosSymmetric_4932m_rotatedZ37d_modified_srtm30_1min']
+gnames += ['OceanOnly_IcosSymmetric_4932m_rotatedZ37d_modified_srtm30_1min']
 
 if not os.path.exists(path_rgrid): 
   os.makedirs(path_rgrid)
@@ -42,10 +43,10 @@ if not os.path.exists(path_sections):
   os.makedirs(path_sections)
 
 for gname in gnames:
-  print(gname)
 
   # --- grids
   sname = 'global_1.0'
+  print(gname+': '+sname)
   if sname in all_grids:
     pyic.ckdtree_hgrid(lon_reg=[-180.,180.], lat_reg=[-90.,90.], res=1.0,
                       fname_tgrid  = gname+'/'+gname+'.nc',
@@ -56,6 +57,7 @@ for gname in gnames:
                       )
   
   sname = 'global_0.3'
+  print(gname+': '+sname)
   if sname in all_grids:
     pyic.ckdtree_hgrid(lon_reg=[-180.,180.], lat_reg=[-90.,90.], res=0.3,
                       fname_tgrid  = gname+'/'+gname+'.nc',
@@ -66,8 +68,20 @@ for gname in gnames:
                       )
   
   sname = 'global_0.1'
+  print(gname+': '+sname)
   if sname in all_grids:
     pyic.ckdtree_hgrid(lon_reg=[-180.,180.], lat_reg=[-90.,90.], res=0.1,
+                      fname_tgrid  = gname+'/'+gname+'.nc',
+                      path_tgrid   = path_tgrid,
+                      path_ckdtree = path_rgrid,
+                      sname = sname,
+                      gname = gname,
+                      )
+  
+  sname = 'global_0.05'
+  print(gname+': '+sname)
+  if sname in all_grids:
+    pyic.ckdtree_hgrid(lon_reg=[-180.,180.], lat_reg=[-90.,90.], res=0.05,
                       fname_tgrid  = gname+'/'+gname+'.nc',
                       path_tgrid   = path_tgrid,
                       path_ckdtree = path_rgrid,
@@ -78,6 +92,7 @@ for gname in gnames:
 
   # --- sections
   sname = '30W_100pts'
+  print(gname+': '+sname)
   if sname in all_secs:
     dckdtree, ickdtree, lon_sec, lat_sec, dist_sec = pyic.ckdtree_section(p1=[-30,-80], p2=[-30,80], npoints=100,
                       fname_tgrid  = gname+'/'+gname+'.nc',
@@ -88,6 +103,7 @@ for gname in gnames:
                       )
 
   sname = '30W_200pts'
+  print(gname+': '+sname)
   if sname in all_secs:
     dckdtree, ickdtree, lon_sec, lat_sec, dist_sec = pyic.ckdtree_section(p1=[-30,-80], p2=[-30,80], npoints=200,
                       fname_tgrid  = gname+'/'+gname+'.nc',
@@ -98,6 +114,7 @@ for gname in gnames:
                       )
     
   sname = '170W_100pts'
+  print(gname+': '+sname)
   if sname in all_secs:
     dckdtree, ickdtree, lon_sec, lat_sec, dist_sec = pyic.ckdtree_section(p1=[-170,-80], p2=[-170,80], npoints=100,
                       fname_tgrid  = gname+'/'+gname+'.nc',
