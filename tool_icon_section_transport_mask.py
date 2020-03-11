daee85a64102655562d14f6a9a505fc2473f3a77
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2020-03-11 11:24:33 +0100

    tool_icon_section_transport_mask.py: new version

--- tool_icon_section_transport_mask.py
+++ tool_icon_section_transport_mask.py
@@ -2,12 +2,27 @@
 import sys
 import numpy as np
 from netCDF4 import Dataset
-from ipdb import set_trace as mybreak
+#from ipdb import set_trace as mybreak
 
 # --- dummy object to collect variables
 class Obj(object):
   def __init__(self):
+    self.along_const_lat = False
     return
+
+def haversine_dist(lon_ref, lat_ref, lon_pts, lat_pts, degree=True):
+  # for details see http://en.wikipedia.org/wiki/Haversine_formula
+  r = 6378.e3
+  if degree:
+    lon_ref = lon_ref * np.pi/180.
+    lat_ref = lat_ref * np.pi/180.
+    lon_pts = lon_pts * np.pi/180.
+    lat_pts = lat_pts * np.pi/180.
+  arg = np.sqrt(   np.sin(0.5*(lat_pts-lat_ref))**2 
+                 + np.sin(0.5*(lon_pts-lon_ref))**2
+                 * np.cos(lat_ref)*np.cos(lat_pts) )
+  dist = 2*r * np.arcsin(arg)
+  return dist
 
 # --- function to load the tripolar grid
 def load_grid(fpath_tgrid):
@@ -16,8 +31,14 @@ def load_grid(fpath_tgrid):
   
   f = Dataset(IcD.fpath_tgrid, 'r')
   IcD.orientation_of_normal = f.variables['orientation_of_normal'][:].transpose()
+  IcD.edge_orientation = f.variables['edge_orientation'][:].transpose()
   IcD.edge_vertices = f.variables['edge_vertices'][:].transpose()-1
   IcD.edge_of_cell = f.variables['edge_of_cell'][:].transpose()-1
+  IcD.vertex_of_cell = f.variables['vertex_of_cell'][:].transpose()-1
+  IcD.vertices_of_vertex = f.variables['vertices_of_vertex'][:].transpose()-1
+  IcD.edges_of_vertex = f.variables['edges_of_vertex'][:].transpose()-1
+  IcD.adjacent_cell_of_edge = f.variables['adjacent_cell_of_edge'][:].transpose()-1
+  IcD.neighbor_cell_index = f.variables['neighbor_cell_index'][:].transpose()-1
   IcD.clon = f.variables['clon'][:]*180./np.pi
   IcD.clat = f.variables['clat'][:]*180./np.pi
   IcD.elon = f.variables['elon'][:]*180./np.pi
@@ -29,7 +50,7 @@ def load_grid(fpath_tgrid):
   return IcD
 
 # --- main function to derive section edge
-def find_section_edges_orientation(IcD, lon1, lat1, lon2, lat2, dlon=10., dlat=10.):
+def find_section_edges_orientation(IcD, lon1, lat1, lon2, lat2, along_const_lat=False):
 
   # --- renaming 
   elon = IcD.elon
@@ -39,99 +60,236 @@ def find_section_edges_orientation(IcD, lon1, lat1, lon2, lat2, dlon=10., dlat=1
   vlon = IcD.vlon
   vlat = IcD.vlat
 
-  # --- define domain
-  if lon1==lon2:
-    if (lat1<lat2):
-      ireg = np.where((clon>=lon1) & (clon<=lon2+dlon) & (clat<=lat2) & (clat>=lat1))[0]
-    elif (lat1>lat2):
-      ireg = np.where((clon>=lon1) & (clon<=lon2+dlon) & (clat>=lat2) & (clat<=lat1))[0]
-  elif lat1==lat2:
-    if (lon1<lon2):
-      ireg = np.where((clon>=lon1) & (clon<=lon2) & (clat<=lat2+dlat) & (clat>=lat1))[0]
+  # --- path along great circle
+  if not along_const_lat:
+    p1 = lon1*np.pi/180.
+    t1 = lat1*np.pi/180.
+    p2 = lon2*np.pi/180.
+    t2 = lat2*np.pi/180.
+    p3 = IcD.vlon*np.pi/180.
+    t3 = IcD.vlat*np.pi/180.
+    crit = ( np.cos(p3)*np.cos(t3)*( np.sin(p1)*np.cos(t1)*np.sin(t2) - np.sin(t1)*np.sin(p2)*np.cos(t2) )
+           + np.sin(p3)*np.cos(t3)*( np.sin(t1)*np.cos(p2)*np.cos(t2) - np.cos(p1)*np.cos(t1)*np.sin(t2) )
+           + np.sin(t3)           *( np.cos(p1)*np.cos(t1)*np.sin(p2)*np.cos(t2) - np.sin(p1)*np.cos(t1)*np.cos(p2)*np.cos(t2) ) )
+    mask_e = crit[IcD.edge_vertices[:,0]]*crit[IcD.edge_vertices[:,1]]<0.
+    # --- cut to longitude range
+    if lon1<lon2:
+      mask_e[ (elon<lon1) | (elon>lon2) ] = False
+    elif lon1>lon2:
+      mask_e[ (elon>lon1) | (elon<lon2) ] = False
+    # --- cut to latitude range if lon1==lon2
     else:
-      ireg = np.where((clon<=lon1) & (clon>=lon2) & (clat<=lat2+dlat) & (clat>=lat1))[0]
-  elif (lon1!=lon2) and (lat1!=lat2):
-    g1 = (lat2-lat1)/(lon2-lon1)*(clon-lon1) + lat1
-    g2 = (lat2-lat1)/(lon2-lon1)*(clon-lon1-dlon) + lat1
-    if   (lon1<lon2) and (lat1<lat2):
-      ireg = np.where((clat<=g1) & (clat<=lat2) & (clat>=g2) & (clat>=lat1))[0]
-    elif (lon1>lon2) and (lat1<lat2):
-      ireg = np.where((clat>=g1) & (clat<=lat2) & (clat<=g2) & (clat>=lat1))[0]
-    elif (lon1<lon2) and (lat1>lat2):
-      ireg = np.where((clat>=g1) & (clat>=lat2) & (clat<=g2) & (clat<=lat1))[0]
-    elif (lon1>lon2) and (lat1>lat2):
-      ireg = np.where((clat<=g1) & (clat>=lat2) & (clat>=g2) & (clat<=lat1))[0]
+      mask_e[ (elat<lat1) | (elat>lat2) ] = False
+      mask_e[ np.abs(elon-lon1)>5. ] = False
+  # --- path along const. latitude
   else:
-    raise ValueError('::: Unappropriate choice of lon1, lon2, lat1, lat2! :::')
+    crit = (vlat-lat1)
+    mask_e = crit[IcD.edge_vertices[:,0]]*crit[IcD.edge_vertices[:,1]]<0.
+    mask_e[ (elon<lon1) | (elon>lon2) ] = False
+  
+  # --- all cells in domain
+  ic = IcD.adjacent_cell_of_edge[mask_e,:]
+  ic = np.unique(ic)
+  ic = ic[ic!=-1]
+  
+  if ic.size==0:
+    raise ValueError('::: Error: No points in domain! :::')
+  
+  # --- all edges that belong to outer perimeter
+  ie, ind, cnts = np.unique(IcD.edge_of_cell[ic,:], return_index=True, return_counts=True)
+  ie = ie[cnts==1]
+  # --- all vertices that belong to perimeter edges
+  iv = 1*IcD.edge_vertices[ie,:]
+  # --- find start and end point
+  iv_start = iv.flat[ np.argmin((vlon[iv]-lon1)**2+(vlat[iv]-lat1)**2) ]
+  iv_end   = iv.flat[ np.argmin((vlon[iv]-lon2)**2+(vlat[iv]-lat2)**2) ]
+  
+  #for kk in range(2):
+  for kk in range(1):
+    iv = 1*IcD.edge_vertices[ie,:]
+    iv_next = iv_start
+    iv_sec = [iv_next]
+    ie_sec = []
+    or_sec = []
+    
+    itmp = np.where((iv==iv_next).sum(axis=1))[0][kk]
+  
+    cont = True
+    start_before_end = False
+    nn = 0
+    while cont:
+      nn+=1
+      # --- save old value
+      iv_previous = iv_next
+      # --- index where vertex list matches iv_previous to find iv_next
+      if nn==1:
+        # --- first time there are two choices acoording to two ways around domain
+        itmp = np.where((iv==iv_next).sum(axis=1))[0][kk]
+      else:
+        # --- after that there should be only one choice
+        itmp = np.where((iv==iv_next).sum(axis=1))[0][0]
+      # --- next edge
+      ie_next = ie[itmp]
+      # --- next vertex
+      iv_next = iv[itmp,iv[itmp,:]!=iv_previous][0]
+      # --- orientation
+      itmp2 = np.where(IcD.vertices_of_vertex[iv_previous,:]==iv_next)[0][0]
+      or_next = IcD.edge_orientation[iv_previous, itmp2]
+      # --- delete iv entry such that it is not found again
+      iv[itmp,:] = np.ma.masked
+      # --- save indices
+      iv_sec.append(iv_next)
+      ie_sec.append(ie_next)
+      or_sec.append(or_next)
+  
+      # --- look for hanging cells
+      ie_tmp = ie[np.isin(ie,IcD.edges_of_vertex[iv_next,:])]
+      if ie_tmp.size>2:
+        print('::: Warning, hanging cell.:::')
+        print(ie_tmp)
+        cont = False
+    
+      # --- check whether... 
+      # --- ... we are at the final destination
+      if iv_next==iv_end:
+        cont = False
+      # --- ... back to the beginning
+      elif iv_next==iv_start:
+        print('::: Warning: Reached start vertex again before reaching end vertex! :::')
+        cont = False
+        start_before_end = True
+      # --- ... have not found anything after X iterations
+      elif nn>20000:
+        print('::: Warning: Max. number of iterations exceeded. :::')
+        cont = False
+    else:
+      if start_before_end:
+        dist_sec = haversine_dist(lon2, lat2, vlon[iv_sec], vlat[iv_sec], degree=True)
+        ddist = np.diff(dist_sec)
+        count = 0.
+        for nn in range(ddist.size):
+          if ddist[nn]>0:  # we are getting farer away from point 2
+            count += 1.
+          else:        # we are getting closer to point 2
+            count = 0.
+          if count>5.:
+            print('Cutting section at point closest to end point.')
+            nn += -5
+            break
+        iv_sec = iv_sec[:nn+1]
+        ie_sec = ie_sec[:nn]
+        or_sec = or_sec[:nn]
+      # --- save the two index lists
+      if kk==0:
+        iv_sec1 = np.array(iv_sec)
+        ie_sec1 = np.array(ie_sec)
+        or_sec1 = np.array(or_sec)
+      else:
+        iv_sec2 = np.array(iv_sec)
+        ie_sec2 = np.array(ie_sec)
+        or_sec2 = np.array(or_sec)
 
-  if ireg.size==0:
-    raise ValueError('::: No points in domain! Choose different points or dlon, dlat.')
-  
-  # --- find all vertices and edges that belong to domain
-  # ------ all edges that belong to the cells of domain
-  iedge = IcD.edge_of_cell[ireg]
-  iedge = iedge.reshape(iedge.size)
-  edge_orient = IcD.orientation_of_normal[ireg] 
-  edge_orient = edge_orient.reshape(iedge.size)
-  # ------ edges that appear only once and which are thus domain boundaries
-  iedge_out, ind, cnts = np.unique(iedge, return_index=True, return_counts=True)
-  iedge_out = iedge_out[cnts==1]
-  edge_orient = edge_orient[ind]
-  edge_orient = edge_orient[cnts==1]
-  
-  # ----- vertices which correspond to edges
-  iv_peri_gd = IcD.edge_vertices[iedge_out]
-  
-  # --- find start edge and vertex
-  # ------ find first candidate
-  ip1 = np.unravel_index(np.argmin((vlon[iv_peri_gd]-lon1)**2+(vlat[iv_peri_gd]-lat1)**2), iv_peri_gd.shape)
-  # ------ find second candidate
-  iv_peri_gd_tmp = 1*iv_peri_gd
-  iv_peri_gd_tmp[ip1[0],:] = -1
-  ip2 = np.unravel_index(np.argmin((vlon[iv_peri_gd_tmp]-lon1)**2+(vlat[iv_peri_gd_tmp]-lat1)**2), iv_peri_gd.shape)
-  # ----- decide between two candidates whether the other edge vertex is closer to end point
-  vlon1 = vlon[iv_peri_gd[ip1[0],1-ip1[1]]]
-  vlat1 = vlat[iv_peri_gd[ip1[0],1-ip1[1]]]
-  vlon2 = vlon[iv_peri_gd[ip2[0],1-ip2[1]]]
-  vlat2 = vlat[iv_peri_gd[ip2[0],1-ip2[1]]]
-  itmp = np.argmin((np.array([vlon1,vlon2])-lon2)**2+(np.array([vlat1,vlat2])-lat2)**2)
-  if itmp==0:
-    ie_d = ip1[0]
-    iv_g = iv_peri_gd[ie_d, ip1[1]]
-    iv_next_g = iv_peri_gd[ie_d, 1-ip1[1]]
-  else:
-    ie_d = ip2[0]
-    iv_g = iv_peri_gd[ie_d, ip2[1]]
-    iv_next_g = iv_peri_gd[ie_d, 1-ip2[1]]
-  
-  # --- go around domain
-  iedge_out_sorted_gd = np.zeros(iedge_out.size, dtype=int)
-  ivert_out_sorted_gd = np.zeros(iedge_out.size+1, dtype=int)
-  edge_orient_sorted = np.zeros(iedge_out.size, dtype=int)
-  iedge_out_sorted_gd[0] = iedge_out[ie_d]
-  ivert_out_sorted_gd[0] = iv_g
-  ivert_out_sorted_gd[1] = iv_next_g
-  edge_orient_sorted[0] = edge_orient[ie_d]
-  for nn in range(iedge_out_sorted_gd.size-1):
-    # ----- delete old vertices such that they are not found again
-    iv_peri_gd[ie_d,:] = -1
-    # ----- find next edge by common vertex between this and next edge
-    ie_d = np.where(iv_peri_gd==iv_next_g)[0]
-    iedge_out_sorted_gd[nn+1] = iedge_out[ie_d]
-    edge_orient_sorted[nn+1] = edge_orient[ie_d]
-    # ----- find next vertex as other vertex of edge
-    ind_01 = np.where(iv_peri_gd==iv_next_g)[1]
-    iv_next_g  = iv_peri_gd[ie_d, 1-ind_01]
-    ivert_out_sorted_gd[nn+2] = iv_next_g
-  
-  # --- reduce perimeter to section by cutting it at final point
-  ie_final_d = np.argmin((vlon[ivert_out_sorted_gd]-lon2)**2+(vlat[ivert_out_sorted_gd]-lat2)**2)
-  iedge_section = iedge_out_sorted_gd[:ie_final_d]
-  ivert_section = ivert_out_sorted_gd[:ie_final_d]
-  edge_orient_section = edge_orient_sorted[:ie_final_d]
 
+  # --- randomly change sec1 for output
   edge_mask = np.zeros((IcD.elon.size))
-  edge_mask[iedge_section] = edge_orient_section
+  edge_mask[ie_sec1] = or_sec1
+
+  return edge_mask
+
+def find_section_edges_orientation_2(IcD, lon1, lat1, lon2, lat2, along_const_lat=False):
+
+  # --- renaming 
+  elon = IcD.elon
+  elat = IcD.elat
+  clon = IcD.clon
+  clat = IcD.clat
+  vlon = IcD.vlon
+  vlat = IcD.vlat
+
+  # --- path along great circle
+  if not along_const_lat:
+    p1 = lon1*np.pi/180.
+    t1 = lat1*np.pi/180.
+    p2 = lon2*np.pi/180.
+    t2 = lat2*np.pi/180.
+    p3 = IcD.vlon*np.pi/180.
+    t3 = IcD.vlat*np.pi/180.
+    crit = ( np.cos(p3)*np.cos(t3)*( np.sin(p1)*np.cos(t1)*np.sin(t2) - np.sin(t1)*np.sin(p2)*np.cos(t2) )
+           + np.sin(p3)*np.cos(t3)*( np.sin(t1)*np.cos(p2)*np.cos(t2) - np.cos(p1)*np.cos(t1)*np.sin(t2) )
+           + np.sin(t3)           *( np.cos(p1)*np.cos(t1)*np.sin(p2)*np.cos(t2) - np.sin(p1)*np.cos(t1)*np.cos(p2)*np.cos(t2) ) )
+    mask_e = crit[IcD.edge_vertices[:,0]]*crit[IcD.edge_vertices[:,1]]<0.
+    # --- cut to longitude range
+    if lon1<lon2:
+      mask_e[ (elon<lon1) | (elon>lon2) ] = False
+    elif lon1>lon2:
+      mask_e[ (elon>lon1) | (elon<lon2) ] = False
+    # --- cut to latitude range if lon1==lon2
+    else:
+      mask_e[ (elat<lat1) | (elat>lat2) ] = False
+      mask_e[ np.abs(elon-lon1)>5. ] = False
+  # --- path along const. latitude
+  else:
+    crit = (vlat-lat1)
+    mask_e = crit[IcD.edge_vertices[:,0]]*crit[IcD.edge_vertices[:,1]]<0.
+    mask_e[ (elon<lon1) | (elon>lon2) ] = False
+
+  # --- all vertices belonging to edges
+  iv_all = IcD.edge_vertices[mask_e,:]
+  # --- delete vertices that only occur once - but not start and end vertex
+  itmp, ind, cnts = np.unique(iv_all, return_index=True, return_counts=True)
+  iv_inval = itmp[cnts==1]
+  iv_s = iv_all.flatten()[np.argmin((vlon[iv_all]-lon1)**2+(vlat[iv_all]-lat1)**2)]
+  iv_e = iv_all.flatten()[np.argmin((vlon[iv_all]-lon2)**2+(vlat[iv_all]-lat2)**2)]
+  iv_inval = iv_inval[iv_inval!=iv_s]
+  iv_inval = iv_inval[iv_inval!=iv_e]
+  # --- all edges
+  ie_all = np.where(mask_e)[0]
+  # --- valid edges are those who are not attached to invalid vertices 
+  mtmp = np.isin(ie_all,IcD.edges_of_vertex[iv_inval])
+  ie_valid = ie_all[mtmp==False]
+  iv_valid = IcD.edge_vertices[ie_valid,:]
+
+  # --- find start and end vertices
+  i, c = np.unique(iv_valid, return_counts=True) 
+  iv_se = i[c==1]
+  iv_start = iv_se[np.argmin((vlon[iv_se]-lon1)**2+(vlat[iv_se]-lat1)**2)]
+  iv_next = iv_start
+  # --- allocate final lists
+  iv_sec = [iv_start]
+  ie_sec = []
+  or_sec = []
+
+  for nn in range(ie_valid.size):
+    # --- save old value
+    iv_previous = iv_next
+    # --- index where vertex list matches iv_next
+    itmp = np.where((iv_valid==iv_next).sum(axis=1))[0][0]
+    # --- next edge
+    ie_next = ie_valid[itmp]
+    # --- next vertex
+    iv_next = iv_valid[itmp,iv_valid[itmp,:]!=iv_previous][0]
+    # --- orientation
+    #itmp2 = np.where(IcD.vertices_of_vertex[iv_previous,:]==iv_next)[0][0]
+    ##itmp2 = np.where((iv_valid==[iv_previous,iv_next]).sum(axis=1))[0]
+    ##if itmp2.size==1:
+    ##  itmp2 = itmp2[0]
+    ##else:
+    ##  itmp2 = np.where((iv_valid==[iv_next,iv_previous]).sum(axis=1))[0][0]
+    itmp2 = np.where(IcD.edges_of_vertex[iv_previous]==ie_next)[0][0]
+    or_next = IcD.edge_orientation[iv_previous, itmp2]
+    # --- delete iv entry such that it is not found again
+    iv_valid[itmp,:] = np.ma.masked
+    # --- save indices
+    iv_sec.append(iv_next)
+    ie_sec.append(ie_next)
+    or_sec.append(or_next)
+
+  #print(np.array(ie_sec).size)
+  #print(ie_sec)
+
+  # --- output
+  edge_mask = np.zeros((IcD.elon.size))
+  edge_mask[ie_sec] = or_sec
 
   return edge_mask
 
@@ -142,24 +300,54 @@ Ms = []
 # START USER INPUT
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 # --- path to triploar grid file
-fpath_tgrid = '/Users/nbruggemann/work/icon_playground/icon_r2b4_test_data/icon_08/icon-oes/experiments/nib0002/OceanOnly_Icos_0158km_etopo40.nc'
+fpath_tgrid = '/pool/data/ICON/oes/input/r0003/OceanOnly_Global_IcosSymmetric_0039km_rotatedZ37d_BlackSea_Greenland_modified_srtm30_1min/OceanOnly_Global_IcosSymmetric_0039km_rotatedZ37d_BlackSea_Greenland_modified_srtm30_1min.nc'
+#fpath_tgrid = '/pool/data/ICON/oes/input/r0002/OceanOnly_Icos_0158km_etopo40/OceanOnly_Icos_0158km_etopo40.nc'
 # --- path to output netcdf file
-fpath_ncfile_out = 'test_icon_bstr_preb.nc'
+fpath_ncfile_out = 'icon_section_transport_mask.nc'
+
+# --- sec 1 barents_opening
+M = Obj()
+#M.lon1, M.lat1 = 16.8, 76.5
+#M.lon2, M.lat2 = 19.2, 70.2
+M.lon1, M.lat1 = 16.6, 77.
+M.lon2, M.lat2 = 19.5, 69.8
+M.name = 'barents_opening'
+Ms.append(M)
+
+# --- sec 2 bering_strait
+M = Obj()
+M.lon1, M.lat1 = -171, 66.2
+M.lon2, M.lat2 = -166, 65
+M.name = 'bering_strait'
+Ms.append(M)
+
+# --- sec 6 drake_passage
+M = Obj()
+#M.lon1, M.lat1 = -68, -54.
+#M.lon1, M.lat1 = -60, -64.7
+#M.lon2, M.lat2 = -68, -54.
+
+M.lon1, M.lat1 = -60.5, -64.3
+M.lon2, M.lat2 = -67, -55.
+M.name = 'drake_passage'
+Ms.append(M)
 
 # --- sec 1
 M = Obj()
-M.lon1, M.lat1 = -110, -20
-M.lon2, M.lat2 = -120, -50
-M.dlon, M.dlat = 10., 10.
-M.name = 'test_section1'
-Ms.append(M)
-
-# --- sec 2
-M = Obj()
-M.lon1, M.lat1 = -110, -20
-M.lon2, M.lat2 = -120, -50
-M.dlon, M.dlat = 10., 10.
-M.name = 'test_section2'
+#M.lon1, M.lat1 = -98., 26.
+M.lon1, M.lat1 = -80.5, 26.
+M.lon2, M.lat2 = -14., 26.
+M.along_const_lat = True
+#M.lon1, M.lat1 = -60., 54.
+#M.lon2, M.lat2 = -43., 70.
+#M.name = '26N'
+#M.lon1, M.lat1 = -50., 0.
+#M.lon2, M.lat2 =  12., 0.
+#M.name = 'Atl.Eq.'
+#M.lon1, M.lat1 = -30., -78.
+#M.lon2, M.lat2 = -30.,  70.
+M.name = 'atl26N'
+#M.along_const_lat = True
 Ms.append(M)
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
@@ -184,8 +372,8 @@ for M in Ms:
   print(M.name)
 
   # --- derive edge_mask
-  edge_mask = find_section_edges_orientation(
-     IcD, M.lon1, M.lat1, M.lon2, M.lat2, dlon=M.dlon, dlat=M.dlat)
+  edge_mask = find_section_edges_orientation_2(
+     IcD, M.lon1, M.lat1, M.lon2, M.lat2, M.along_const_lat)
 
   # --- save edge_mask to nc file
   ncv = fo.createVariable(M.name,'f4',('edge',))
@@ -193,4 +381,56 @@ for M in Ms:
 
 # --- close nc file
 fo.close()
+
+if True:
+  import pyicon as pyic
+  import matplotlib.pyplot as plt
+  import matplotlib
+  import my_toolbox as my
+  fo = Dataset(fpath_ncfile_out, 'r')
+  vlist = np.array(list(fo.variables.keys()))
+  vlist=vlist[vlist!='elon']
+  vlist=vlist[vlist!='elat']
+  for var in fo.variables.keys():
+    exec('%s = fo.variables[var][:]'%var)
+  fo.close()
+
+  elon = IcD.elon
+  elat = IcD.elat
+  clon = IcD.clon
+  clat = IcD.clat
+  vlon = IcD.vlon
+  vlat = IcD.vlat
+
+  # --- for plotting the grid
+  IcD.Tri = matplotlib.tri.Triangulation(IcD.vlon, IcD.vlat, 
+                                          triangles=IcD.vertex_of_cell)
+  mask_bt = (
+    (IcD.vlon[IcD.vertex_of_cell[:,0]] - IcD.vlon[IcD.vertex_of_cell[:,1]])**2
+  + (IcD.vlon[IcD.vertex_of_cell[:,0]] - IcD.vlon[IcD.vertex_of_cell[:,2]])**2
+  + (IcD.vlat[IcD.vertex_of_cell[:,0]] - IcD.vlat[IcD.vertex_of_cell[:,1]])**2
+  + (IcD.vlat[IcD.vertex_of_cell[:,0]] - IcD.vlat[IcD.vertex_of_cell[:,2]])**2
+             ) > 2.*180./np.pi
+  IcD.Tri.set_mask(mask_bt)
+  
+  empt_data = np.zeros(IcD.clon.shape)
+
+  plt.close("all")
+  hca, hcb = pyic.arrange_axes(1,1, plot_cb=True, sasp=0.5, fig_size_fac=2.,
+                              sharex=True, sharey=True, xlabel="", ylabel="")
+  ii=-1
+  
+  ii+=1; ax=hca[ii]; cax=hcb[ii]
+  #ax.plot([lon1, lon2], [lat1, lat2])
+  pyic.trishade(IcD.Tri, empt_data, ax=ax, cax=cax, edgecolor='k', clim=1)
+  #for var in vlist:
+  for M in Ms:
+    var = M.name
+    print(var)
+    exec('mask = 1.*%s'%var)
+    ax.scatter(elon[mask!=0], elat[mask!=0], c='b', s=10)
+    ax.scatter(M.lon1, M.lat1, c='g', s=20)
+    ax.scatter(M.lon2, M.lat2, c='r', s=20)
+
+  plt.show()
 
