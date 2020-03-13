7d1ea31a8f56bc7fc01e5117615ac05e3dd05c11
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2020-03-13 17:06:04 +0100

    tools/conda_act_pyicon_env.sh: Added this file to easily setup pyicon compatible python env.

---
+++ tools/conda_act_pyicon_env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# --- conda env settings
+# script to working conda environment
+path_conda_bin="/home/mpim/m300602/miniconda2/bin/"
+# name of working conda environment
+conda_env="myenv_py3"
+
+# --- add pyicon to PYTHONPATH
+PYICON_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+export PYTHONPATH="${PYICON_PATH}"
+# use this if you have a harmless PYTHONPATH which you want to keep
+#export PYTHONPATH="${PYICON_PATH}:${PYTHONPATH}"
+echo "PYTHONPATH was modified to:" 
+echo "${PYTHONPATH}"
+echo ""
+
+# --- activate conda environment
+echo "Activate conda environment by:"
+echo "${path_conda_bin}/activate myenv_py3"
+source ${path_conda_bin}/activate ${conda_env}
+echo ""
+
+# --- print some information
+conda_path=`which conda`
+echo "Active conda:"
+echo "${conda_path}"
+python_path=`which python`
+echo "Active python:"
+echo "${python_path}"
