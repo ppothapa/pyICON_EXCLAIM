a81a36f418b4830de824e066da901e84c8a868f7
nbruegge <nils.brueggemann@uni-hamburg.de>
2020-03-24 20:15:10 +0100

    pyicon/pyicon_plotting.py: Added new version of arrange_axes. Modifications/ expansions in plot_settings.

--- pyicon/pyicon_plotting.py
+++ pyicon/pyicon_plotting.py
@@ -269,13 +269,15 @@ def shade(
               contlw=1.,
               use_pcol=True,
               cbticks='auto',
+              cbtitle='',
+              cbdrawedges='auto',
+              #cborientation='vertical',
+              cborientation='auto',
               adjust_axlims=True,
               bmp=None,
               transform=None,
               logplot=False,
-              cbtitle='',
               edgecolor='none',
-              cbdrawedges='auto',
            ):
     """ Convenient wrapper around pcolormesh, contourf, contour and their triangular versions.
     """
@@ -471,8 +473,14 @@ def shade(
         from mpl_toolkits.axes_grid1 import make_axes_locatable
         div = make_axes_locatable(ax)
         cax = div.append_axes("right", size="10%", pad=0.1)
+      # ------ determine cborientation
+      if cborientation=='auto':
+        if cax.get_xticks().size==0:
+          cborientation = 'vertical'
+        else:
+          cborientation = 'horizontal'
       # ------ make actual colorbar
-      cb = plt.colorbar(mappable=hm, cax=cax, extend=extend)
+      cb = plt.colorbar(mappable=hm, cax=cax, extend=extend, orientation=cborientation)
       # ------ prevent white lines if fig is saved as pdf
       cb.solids.set_edgecolor("face")
       # ------ use exponential notation for large colorbar ticks
@@ -703,7 +711,7 @@ last change:
 
 # ================================================================================ 
 
-def arrange_axes( nx,ny,
+def arrange_axes_old( nx,ny,
                   # height of and aspect ratio of subplot
                   asy  = 3.5,
                   sasp = 0.5,
@@ -907,6 +915,322 @@ last change:
     #plotsettings(hca,hcb)
     return hca, hcb
 
+def arrange_axes(nx,ny,
+                 sharex = True,
+                 sharey = False,
+                 xlabel = '',
+                 ylabel = '',
+                 # labeling axes with e.g. (a), (b), (c)
+                 do_axes_labels = True,
+                 axlab_kw = dict(),
+                 # colorbar
+                 plot_cb = True,
+                 # projection (e.g. for cartopy)
+                 projection = None,
+                 # aspect ratio of axes
+                 asp = 1.,
+                 # width and height of axes
+                 wax = 'auto',
+                 hax = 4.,
+                 # extra figure spaces (left, right, top, bottom)
+                 dfigl = 0.0,
+                 dfigr = 0.0,
+                 dfigt = 0.0,
+                 dfigb = 0.0,
+                 # space aroung axes (left, right, top, bottom) 
+                 daxl = 1.8, # reset to zero if sharex==False
+                 daxr = 0.8,
+                 daxt = 0.8,
+                 daxb = 1.2, # reset to zero if sharex==True
+                 # space around colorbars (left, right, top, bottom) 
+                 dcbl = -0.5,
+                 dcbr = 1.2,
+                 dcbt = 0.0,
+                 dcbb = 0.5,
+                 # width and height of colorbars
+                 wcb = 0.5,
+                 hcb = 'auto',
+                 # factors to increase widths and heights of axes and colorbars
+                 fig_size_fac = 1.,
+                 f_wax = 1.,
+                 f_hax = 1.,
+                 f_wcb = 1.,
+                 f_hcb = 1.,
+                 # factors to increase spaces (figure)
+                 f_dfigl = 1.,
+                 f_dfigr = 1.,
+                 f_dfigt = 1.,
+                 f_dfigb = 1.,
+                 # factors to increase spaces (axes)
+                 f_daxl = 1.,
+                 f_daxr = 1.,
+                 f_daxt = 1.,
+                 f_daxb = 1.,
+                 # factors to increase spaces (colorbars)
+                 f_dcbl = 1.,
+                 f_dcbr = 1.,
+                 f_dcbt = 1.,
+                 f_dcbb = 1.,
+                 # font sizes of labels, titles, ticks
+                 fs_label = 10.,
+                 fs_title = 12.,
+                 fs_ticks = 10.,
+                 # font size increasing factor
+                 f_fs = 1,
+                ):
+
+  # factor to convert cm into inch
+  cm2inch = 0.3937
+
+  # --- set hcb in case it is auto
+  if isinstance(wax, str) and wax=='auto':
+    wax = hax/asp
+
+  # --- set hcb in case it is auto
+  if isinstance(hcb, str) and hcb=='auto':
+    hcb = hax
+
+  # --- rename horizontal->bottom and vertical->right
+  if isinstance(plot_cb, str) and plot_cb=='horizontal':
+    plot_cb = 'bottom'
+  if isinstance(plot_cb, str) and plot_cb=='vertical':
+    plot_cb = 'right'
+  
+  # --- apply fig_size_fac
+  # font sizes
+  #f_fs *= fig_size_fac
+  # factors to increase widths and heights of axes and colorbars
+  f_wax *= fig_size_fac
+  f_hax *= fig_size_fac
+  #f_wcb *= fig_size_fac
+  f_hcb *= fig_size_fac
+  ## factors to increase spaces (figure)
+  #f_dfigl *= fig_size_fac
+  #f_dfigr *= fig_size_fac
+  #f_dfigt *= fig_size_fac
+  #f_dfigb *= fig_size_fac
+  ## factors to increase spaces (axes)
+  #f_daxl *= fig_size_fac
+  #f_daxr *= fig_size_fac
+  #f_daxt *= fig_size_fac
+  #f_daxb *= fig_size_fac
+  ## factors to increase spaces (colorbars)
+  #f_dcbl *= fig_size_fac
+  #f_dcbr *= fig_size_fac
+  #f_dcbt *= fig_size_fac
+  #f_dcbb *= fig_size_fac
+  
+  # --- apply font size factor
+  fs_label *= f_fs
+  fs_title *= f_fs
+  fs_ticks *= f_fs
+
+  # make vector of plot_cb if it has been true or false before
+  # plot_cb can have values [{1}, 0] 
+  # with meanings:
+  #   1: plot cb; 
+  #   0: do not plot cb
+  plot_cb_right  = False
+  plot_cb_bottom = False
+  if isinstance(plot_cb, bool) and (plot_cb==True):
+    plot_cb = np.ones((nx,ny))  
+  elif isinstance(plot_cb, bool) and (plot_cb==False):
+    plot_cb = np.zeros((nx,ny))
+  elif isinstance(plot_cb, str) and plot_cb=='right':
+    plot_cb = np.zeros((nx,ny))
+    plot_cb_right = True
+  elif isinstance(plot_cb, str) and plot_cb=='bottom':
+    plot_cb = np.zeros((nx,ny))
+    plot_cb_bottom = True
+  else:
+    plot_cb = np.array(plot_cb)
+    if plot_cb.size!=nx*ny:
+      raise ValueError('Vector plot_cb has wrong length!')
+    if plot_cb.shape[0]==nx*ny:
+      plot_cb = plot_cb.reshape(ny,nx).transpose()
+    elif plot_cb.shape[0]==ny:
+      plot_cb = plot_cb.transpose()
+  
+  # --- make list of projections if it is not a list
+  if not isinstance(projection, list):
+    projection = [projection]*nx*ny
+  
+  # --- make arrays and multiply by f_*
+  daxl = np.array([daxl]*nx)*f_daxl
+  daxr = np.array([daxr]*nx)*f_daxr
+  dcbl = np.array([dcbl]*nx)*f_dcbl
+  dcbr = np.array([dcbr]*nx)*f_dcbr
+  
+  wax = np.array([wax]*nx)*f_wax
+  wcb = np.array([wcb]*nx)*f_wcb
+  
+  daxt = np.array([daxt]*ny)*f_daxt
+  daxb = np.array([daxb]*ny)*f_daxb
+  dcbt = np.array([dcbt]*ny)*f_dcbt
+  dcbb = np.array([dcbb]*ny)*f_dcbb
+  
+  hax = np.array([hax]*ny)*f_hax
+  hcb = np.array([hcb]*ny)*f_hcb
+  
+  # --- adjust for shared axes
+  if sharex:
+    daxb[:-1] = 0.
+  
+  if sharey:
+    daxl[1:] = 0.
+
+  # --- adjust for one colorbar at the right or bottom
+  if plot_cb_right:
+    daxr_s = daxr[0]
+    dcbl_s = dcbl[0]
+    dcbr_s = dcbr[0]
+    wcb_s  = wcb[0]
+    hcb_s  = hcb[0]
+    dfigr += dcbl_s+wcb_s+0.*dcbr_s+daxl[0]
+  if plot_cb_bottom:
+    hcb_s  = wcb[0]
+    wcb_s  = wax[0]
+    dcbb_s = dcbb[0]+daxb[-1]
+    dcbt_s = dcbt[0]
+    #hcb_s  = hcb[0]
+    dfigb += dcbb_s+hcb_s+dcbt_s
+  
+  # --- adjust for columns without colorbar
+  delete_cb_space = plot_cb.sum(axis=1)==0
+  dcbl[delete_cb_space] = 0.0
+  dcbr[delete_cb_space] = 0.0
+  wcb[delete_cb_space]  = 0.0
+  
+  # --- determine ax position and fig dimensions
+  x0 =   dfigl
+  y0 = -(dfigt)
+  
+  pos_axcm = np.zeros((nx*ny,4))
+  pos_cbcm = np.zeros((nx*ny,4))
+  nn = -1
+  y00 = y0
+  x00 = x0
+  for jj in range(ny):
+    y0   += -(daxt[jj]+hax[jj])
+    x0 = x00
+    for ii in range(nx):
+      nn += 1
+      x0   += daxl[ii]
+      pos_axcm[nn,:] = [x0, y0, wax[ii], hax[jj]]
+      pos_cbcm[nn,:] = [x0+wax[ii]+daxr[ii]+dcbl[ii], y0, wcb[ii], hcb[jj]]
+      x0   += wax[ii]+daxr[ii]+dcbl[ii]+wcb[ii]+dcbr[ii]
+    y0   += -(daxb[jj])
+  wfig = x0+dfigr
+  hfig = y0-dfigb
+  
+  # --- transform from negative y axis to positive y axis
+  hfig = -hfig
+  pos_axcm[:,1] += hfig
+  pos_cbcm[:,1] += hfig
+  
+  # --- convert to fig coords
+  cm2fig_x = 1./wfig
+  cm2fig_y = 1./hfig
+  
+  pos_ax = 1.*pos_axcm
+  pos_cb = 1.*pos_cbcm
+  
+  pos_ax[:,0] = pos_axcm[:,0]*cm2fig_x
+  pos_ax[:,2] = pos_axcm[:,2]*cm2fig_x
+  pos_ax[:,1] = pos_axcm[:,1]*cm2fig_y
+  pos_ax[:,3] = pos_axcm[:,3]*cm2fig_y
+  
+  pos_cb[:,0] = pos_cbcm[:,0]*cm2fig_x
+  pos_cb[:,2] = pos_cbcm[:,2]*cm2fig_x
+  pos_cb[:,1] = pos_cbcm[:,1]*cm2fig_y
+  pos_cb[:,3] = pos_cbcm[:,3]*cm2fig_y
+
+  # --- find axes center (!= figure center)
+  x_ax_cent = pos_axcm[0,0] +0.5*(pos_axcm[-1,0]+pos_axcm[-1,2]-pos_axcm[0,0])
+  y_ax_cent = pos_axcm[-1,1]+0.5*(pos_axcm[0,1] +pos_axcm[0,3] -pos_axcm[-1,1])
+  
+  # --- make figure and axes
+  fig = plt.figure(figsize=(wfig*cm2inch, hfig*cm2inch))
+  
+  hca = [0]*(nx*ny)
+  hcb = [0]*(nx*ny)
+  nn = -1
+  for jj in range(ny):
+    for ii in range(nx):
+      nn+=1
+  
+      # --- axes
+      hca[nn] = fig.add_subplot(position=pos_ax[nn,:], projection=projection[nn])
+  
+      # --- colorbar
+      if plot_cb[ii,jj] == 1:
+        hcb[nn] = fig.add_subplot(position=pos_cb[nn,:])
+      ax  = hca[nn]
+      cax = hcb[nn] 
+  
+      # --- label
+      ax.set_xlabel(xlabel, fontsize=fs_label)
+      ax.set_ylabel(ylabel, fontsize=fs_label)
+      #ax.set_title('', fontsize=fs_title)
+      matplotlib.rcParams['axes.titlesize'] = fs_title
+      ax.tick_params(labelsize=fs_ticks)
+      if plot_cb[ii,jj] == 1:
+        hcb[nn].tick_params(labelsize=fs_ticks)
+  
+      #ax.tick_params(pad=-10.0)
+      #ax.xaxis.labelpad = 0
+      #ax._set_title_offset_trans(float(-20))
+  
+      # --- axes ticks
+      # delete labels for shared axes
+      if sharex and jj!=ny-1:
+        hca[nn].ticklabel_format(axis='x',style='plain',useOffset=False)
+        hca[nn].tick_params(labelbottom=False)
+        hca[nn].set_xlabel('')
+  
+      if sharey and ii!=0:
+        hca[nn].ticklabel_format(axis='y',style='plain',useOffset=False)
+        hca[nn].tick_params(labelleft=False)
+        hca[nn].set_ylabel('')
+  
+      # ticks for colorbar 
+      if plot_cb[ii,jj] == 1:
+        hcb[nn].set_xticks([])
+        hcb[nn].yaxis.tick_right()
+        hcb[nn].yaxis.set_label_position("right")
+
+  #--- needs to converted to fig coords (not cm)
+  if plot_cb_right:
+    nn = -1
+    #pos_cb = np.array([(wfig-(dfigr+dcbr_s+wcb_s))*cm2fig_x, (y_ax_cent-0.5*hcb_s)*cm2fig_y, wcb_s*cm2fig_x, hcb_s*cm2fig_y])
+    pos_cb = np.array([ (pos_axcm[-1,0]+pos_axcm[-1,2]+daxr_s+dcbl_s)*cm2fig_x, 
+                        (y_ax_cent-0.5*hcb_s)*cm2fig_y, 
+                        (wcb_s)*cm2fig_x, 
+                        (hcb_s)*cm2fig_y 
+                      ])
+    hcb[nn] = fig.add_subplot(position=pos_cb)
+    hcb[nn].tick_params(labelsize=fs_ticks)
+    hcb[nn].set_xticks([])
+    hcb[nn].yaxis.tick_right()
+    hcb[nn].yaxis.set_label_position("right")
+
+  if plot_cb_bottom:
+    nn = -1
+    pos_cb = np.array([ (x_ax_cent-0.5*wcb_s)*cm2fig_x, 
+                        (dcbb_s)*cm2fig_y, 
+                        (wcb_s)*cm2fig_x, 
+                        (hcb_s)*cm2fig_y
+                      ])
+    hcb[nn] = fig.add_subplot(position=pos_cb)
+    hcb[nn].tick_params(labelsize=fs_ticks)
+    hcb[nn].set_yticks([])
+
+  # add letters for subplots
+  if (do_axes_labels) and (axlab_kw is not None):
+    hca = axlab(hca, fontdict=axlab_kw)
+
+  return hca, hcb
+
 # ================================================================================ 
 def axlab(hca, figstr=[], posx=[-0.0], posy=[1.08], fontdict=None):
   """
@@ -929,33 +1253,40 @@ last change:
     for nn, ax in enumerate(hca):
       figstr[nn] = "(%s)" % (lett[nn])
   
-  if len(posx)==1:
-    posx = posx*len(hca)
-  if len(posy)==1:
-    posy = posy*len(hca)
-  
-  # draw text
+  #if len(posx)==1:
+  #  posx = posx*len(hca)
+  #if len(posy)==1:
+  #  posy = posy*len(hca)
+  #
+  ## draw text
+  #for nn, ax in enumerate(hca):
+  #  ht = hca[nn].text(posx[nn], posy[nn], figstr[nn], 
+  #                    transform = hca[nn].transAxes, 
+  #                    horizontalalignment = 'right',
+  #                    fontdict=fontdict)
+  #  # add text handle to axes to give possibility of changing text properties later
+  #  # e.g. by hca[nn].axlab.set_fontsize(8)
+  #  hca[nn].axlab = ht
   for nn, ax in enumerate(hca):
-    ht = hca[nn].text(posx[nn], posy[nn], figstr[nn], 
-                      transform = hca[nn].transAxes, 
-                      horizontalalignment = 'right',
-                      fontdict=fontdict)
-    # add text handle to axes to give possibility of changing text properties later
-    # e.g. by hca[nn].axlab.set_fontsize(8)
-    hca[nn].axlab = ht
+    ax.set_title(figstr[nn], loc='left', fontsize=10)
   return hca
 
 def plot_settings(ax, xlim='none', ylim='none', xticks='auto', yticks='auto', 
                      ticks_position='both', template='none', 
+                     x_minor_tick_diff='none', y_minor_tick_diff='none',
                      # cartopy specific settings
                      projection=None, 
-                     coastlines_color='none', coastlines_resolution='110m',
+                     coastlines_color='k', coastlines_resolution='110m',
                      land_zorder=2, land_facecolor='0.7'):
 
   # --- templates
   if template=='global':
     xlim = [-180,180]
     ylim = [-90,90]
+    xticks = np.arange(-120,121,60.)
+    yticks = np.arange(-60,61,30.)
+    x_minor_tick_diff = 20.
+    y_minor_tick_diff = 10.
   elif template=='na':
     xlim = [-80,0]
     ylim = [30,70]
@@ -993,12 +1324,31 @@ def plot_settings(ax, xlim='none', ylim='none', xticks='auto', yticks='auto',
     ax.xaxis.set_ticks_position('both')
     ax.yaxis.set_ticks_position('both')
 
+  # --- minor ticks
+  if (isinstance(x_minor_tick_diff,str) and x_minor_tick_diff!='auto'):
+    x_minor_tick_diff = (xticks[1]-xticks[0])/5.
+  if (isinstance(y_minor_tick_diff,str) and y_minor_tick_diff!='auto'):
+    y_minor_tick_diff = (yticks[1]-yticks[0])/5.
+  if not (isinstance(x_minor_tick_diff,str) and x_minor_tick_diff!='none'):
+    ax.xaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(x_minor_tick_diff))
+  if not (isinstance(y_minor_tick_diff,str) and y_minor_tick_diff!='none'):
+    ax.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(y_minor_tick_diff))
+
   # --- cartopy specific stuff
   if not projection is None: 
+    #ax.xaxis.set_major_formatter(cartopy.mpl.ticker.LongitudeFormatter(degree_symbol=''))
+    #ax.yaxis.set_major_formatter(cartopy.mpl.ticker.LatitudeFormatter(degree_symbol=''))
     ax.xaxis.set_major_formatter(cartopy.mpl.ticker.LongitudeFormatter())
-    ax.yaxis.set_major_formatter(cartopy.mpl.ticker.LatitudeFormatter() )
-    ax.add_feature(cartopy.feature.LAND, zorder=land_zorder, facecolor=land_facecolor)
+    ax.yaxis.set_major_formatter(cartopy.mpl.ticker.LatitudeFormatter())
+    #for tick in ax.xaxis.get_ticklabels()+ax.yaxis.get_ticklabels():
+    #  tick.set_fontsize(8)
+    feature = cartopy.feature.LAND
+    #feature = feature.with_scale(coastlines_resolution)
+    ax.add_feature(feature, zorder=land_zorder, facecolor=land_facecolor)
     if isinstance(coastlines_color, str) and coastlines_color!='none':
-      ax.coastlines(color=coastlines_color, resolution=coastlines_resolution)
+      #ax.coastlines(color=coastlines_color, resolution=coastlines_resolution)
+      feature = cartopy.feature.COASTLINE
+      #feature = feature.with_scale(coastlines_resolution)
+      ax.add_feature(feature, zorder=land_zorder, edgecolor=coastlines_color)
   return
 
