21c6628f03f2296e214532d34d5c8d57ea4cb13e
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2020-04-06 10:18:36 +0200

    doc/devtool_doc2modvis.py: Update also doc in all_qps to avoid that doc is deleted everytime all_qps is put to server.

--- doc/devtool_doc2modvis.py
+++ doc/devtool_doc2modvis.py
@@ -4,3 +4,4 @@ make html
 
 #rsync -rhv --delete --update doc/_build/html modvis.dkrz.de:/var/www/projects/mh0033/m300602/pyicon_doc
 rsync -rhv --delete --update ./_build/html modvis.dkrz.de:/var/www/projects/mh0033/m300602/pyicon_doc
+rsync -rhv --delete --update ./_build/html ../all_qps/pyicon_doc
