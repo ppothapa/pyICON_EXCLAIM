d9309c32c34d5c39bb9bf04ea53c0c5c741c4e0d
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2020-04-30 11:10:15 +0200

    conda_act_mistral_pyicon_env.sh: Need to go back to myenv_py3 since pyicon_py38 seems not to work anymore with ipython.

--- tools/conda_act_mistral_pyicon_env.sh
+++ tools/conda_act_mistral_pyicon_env.sh
@@ -9,8 +9,8 @@ path_conda_bin="/home/mpim/m300602/miniconda2/bin/"
 #conda_env="pyicon_env3"
 #conda_env="pyicon_env4"
 #conda_env="pyicon_py37"
-conda_env="pyicon_py38"
-#conda_env="myenv_py3"
+#conda_env="pyicon_py38"
+conda_env="myenv_py3"
 
 # --- add pyicon to PYTHONPATH
 PYICON_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
