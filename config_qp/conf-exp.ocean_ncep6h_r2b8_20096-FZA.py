3b5e5141ea441a2d55a8ba1df65b87eb93a544ce
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2020-06-11 18:45:26 +0200

    config_qp/conf-exp.ocean_ncep6h_r2b8_20096-FZA.py: Added new config file for r2b8.

---
+++ config_qp/conf-exp.ocean_ncep6h_r2b8_20096-FZA.py
@@ -0,0 +1,23 @@
+runname = ''
+run     = 'exp.ocean_ncep6h_r2b8_20096-FZA'
+gname   = 'r2b8'
+lev     = 'L128'
+#tstep   = '17800101T000000Z'
+tstep   = '????????????????'  # use this line for all data
+
+path_data    = '/work/mh0287/users/helmuth/icon-oes-1.3.00/experiments/'+run+'/'
+path_grid    = '/mnt/lustre01/work/mh0033/m300602/icon/grids/'+gname+'/'
+path_ckdtree = 'auto'
+
+fpath_initial_state = 'auto'
+fpath_tgrid  = 'auto'
+fpath_fx     = 'auto'
+
+oce_def = ''
+oce_moc = '_oce_moc'
+oce_mon = '_oce_mon'
+oce_monthly = ''
+
+tave_ints = [
+['1948-03-01', '1949-02-01'],
+]
