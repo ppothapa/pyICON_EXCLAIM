7ccb2886c073e601574525b696cad24308818324
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2020-07-08 17:37:21 +0200

    config_ckdtree/config_ckdtree_smt20km.py: Also do v-/e-grid and also to 300pts sections.

--- config_ckdtree/config_ckdtree_smt20km.py
+++ config_ckdtree/config_ckdtree_smt20km.py
@@ -23,9 +23,10 @@ all_grids = [
             ]
 
 all_secs = [
-  '30W_100pts',
   '30W_200pts',
-  '170W_100pts',
+  '170W_200pts',
+  '30W_300pts',
+  '170W_300pts',
             ]
 
 #all_grids = []
@@ -40,7 +41,7 @@ if not os.path.exists(path_sections):
 
 for gname in gnames:
   ts = pyic.timing(ts, gname)
-  #print(gname)
+  print(gname)
 
   # --- grids
   sname = 'global_1.0'
@@ -52,50 +53,6 @@ for gname in gnames:
                       sname = sname,
                       gname = gname,
                       tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
-                      )
-
-  sname = 'global_0.3_nn500'
-  if sname in all_grids:
-    pyic.ckdtree_hgrid(lon_reg=[-180.,180.], lat_reg=[-90.,90.], res=0.3,
-                      fname_tgrid  = fname_tgrid,
-                      path_tgrid   = path_tgrid,
-                      path_ckdtree = path_rgrid,
-                      sname = sname,
-                      gname = gname,
-                      tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
-                      n_nearest_neighbours=500,
-                      )
-
-  sname = 'global_0.3_nn20'
-  if sname in all_grids:
-    pyic.ckdtree_hgrid(lon_reg=[-180.,180.], lat_reg=[-90.,90.], res=0.3,
-                      fname_tgrid  = fname_tgrid,
-                      path_tgrid   = path_tgrid,
-                      path_ckdtree = path_rgrid,
-                      sname = sname,
-                      gname = gname,
-                      tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
-                      n_nearest_neighbours=20,
-                      )
-  
-  sname = 'global_0.3_nn5'
-  if sname in all_grids:
-    pyic.ckdtree_hgrid(lon_reg=[-180.,180.], lat_reg=[-90.,90.], res=0.3,
-                      fname_tgrid  = fname_tgrid,
-                      path_tgrid   = path_tgrid,
-                      path_ckdtree = path_rgrid,
-                      sname = sname,
-                      gname = gname,
-                      tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
-                      n_nearest_neighbours=5,
                       )
   
   sname = 'global_0.3'
@@ -107,8 +64,6 @@ for gname in gnames:
                       sname = sname,
                       gname = gname,
                       tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
                       )
   
   sname = 'global_0.1'
@@ -120,8 +75,6 @@ for gname in gnames:
                       sname = sname,
                       gname = gname,
                       tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
                       )
 
   sname = 'global_0.02'
@@ -133,25 +86,10 @@ for gname in gnames:
                       sname = sname,
                       gname = gname,
                       tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
                       )
   
 
   # --- sections
-  sname = '30W_100pts'
-  if sname in all_secs:
-    dckdtree, ickdtree, lon_sec, lat_sec, dist_sec = pyic.ckdtree_section(p1=[-30,-80], p2=[-30,80], npoints=100,
-                      fname_tgrid  = fname_tgrid,
-                      path_tgrid   = path_tgrid,
-                      path_ckdtree = path_sections,
-                      sname = sname,
-                      gname = gname,
-                      tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
-                      )
-
   sname = '30W_200pts'
   if sname in all_secs:
     dckdtree, ickdtree, lon_sec, lat_sec, dist_sec = pyic.ckdtree_section(p1=[-30,-80], p2=[-30,80], npoints=200,
@@ -161,21 +99,39 @@ for gname in gnames:
                       sname = sname,
                       gname = gname,
                       tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
                       )
     
-  sname = '170W_100pts'
+  sname = '170W_200pts'
   if sname in all_secs:
-    dckdtree, ickdtree, lon_sec, lat_sec, dist_sec = pyic.ckdtree_section(p1=[-170,-80], p2=[-170,80], npoints=100,
+    dckdtree, ickdtree, lon_sec, lat_sec, dist_sec = pyic.ckdtree_section(p1=[-170,-80], p2=[-170,80], npoints=200,
                       fname_tgrid  = fname_tgrid,
                       path_tgrid   = path_tgrid,
                       path_ckdtree = path_sections,
                       sname = sname,
                       gname = gname,
                       tgname = tgname,
-                      load_egrid=False,
-                      load_vgrid=False,
+                      )
+
+  sname = '30W_300pts'
+  if sname in all_secs:
+    dckdtree, ickdtree, lon_sec, lat_sec, dist_sec = pyic.ckdtree_section(p1=[-30,-80], p2=[-30,80], npoints=300,
+                      fname_tgrid  = fname_tgrid,
+                      path_tgrid   = path_tgrid,
+                      path_ckdtree = path_sections,
+                      sname = sname,
+                      gname = gname,
+                      tgname = tgname,
+                      )
+    
+  sname = '170W_300pts'
+  if sname in all_secs:
+    dckdtree, ickdtree, lon_sec, lat_sec, dist_sec = pyic.ckdtree_section(p1=[-170,-80], p2=[-170,80], npoints=300,
+                      fname_tgrid  = fname_tgrid,
+                      path_tgrid   = path_tgrid,
+                      path_ckdtree = path_sections,
+                      sname = sname,
+                      gname = gname,
+                      tgname = tgname,
                       )
 
 print('make_ckdtree.py: All done!')
