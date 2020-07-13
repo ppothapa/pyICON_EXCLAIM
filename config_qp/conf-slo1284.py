f1c9a2f81e71aa3b519db5e7d9a0e61da1f35b08
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2020-07-13 15:23:14 +0200

    config_qp/conf-slo1284.py: Adjusted following new calling strategy.

--- config_qp/conf-slo1284.py
+++ config_qp/conf-slo1284.py
@@ -1,38 +1,45 @@
+# --- runname and path to data
 runname   = ''
 run       = 'slo1284'
+path_data     = '/work/mh0469/m211032/Icon/Git_Icon/icon.oes.20200506/experiments/'+run+'/'
 
+# --- do ocean and/or atmosphere plots
+do_atmosphere_plots = True
+do_ocean_plots      = False
+
+# --- grid information
 gname     = 'r2b6'
 lev       = 'L64'
 gname_atm = 'r2b4a'
 lev_atm   = 'L84'
 
-do_atmosphere_plots = True
+# --- path to interpolation files
+path_grid        = '/mnt/lustre01/work/mh0033/m300602/icon/grids/'+gname+'/'
+path_grid_atm    = '/mnt/lustre01/work/mh0033/m300602/icon/grids/'+gname_atm+'/'
+path_ckdtree     = path_grid+'/ckdtree/'
+path_ckdtree_atm = path_grid_atm+'/ckdtree/'
 
-#tstep    = '17800101T000000Z'
-tstep     = '????????????????'  # use this line for all data
+# --- grid files and reference data
+path_pool_oce       = '/pool/data/ICON/oes/input/r0004/OceanOnly_Global_IcosSymmetric_0039km_rotatedZ37d_BlackSea_Greenland_modified_srtm30_1min/'
+gnameu = gname.upper()
+fpath_tgrid         = path_pool_oce + gnameu+'_ocean-grid.nc'
+fpath_tgrid_atm     = '/pool/data/ICON/grids/public/mpim/0013/icon_grid_0013_R02B04_G.nc'
+fpath_ref_data_oce  = path_pool_oce + gnameu+lev+'_initial_state.nc'
+fpath_ref_data_atm  = '/mnt/lustre01/work/mh0033/m300602/icon/era/pyicon_prepare_era.nc'
+fpath_fx            = path_pool_oce + gnameu+lev+'_fx.nc'
 
-#path_data     = '/work/mh0287/users/juergen/icon-oes/experiments/'+run+'/'
-path_data     = '/work/mh0469/m211032/Icon/Git_Icon/icon.oes.20200506/experiments/'+run+'/'
-path_grid     = '/mnt/lustre01/work/mh0033/m300602/icon/grids/'+gname+'/'
-path_grid_atm = '/mnt/lustre01/work/mh0033/m300602/icon/grids/'+gname_atm+'/'
-path_ckdtree  = 'auto'
-
-fpath_initial_state = '/mnt/lustre01/work/mh0033/m300602/proj_vmix/icon/icon_08/icon-oes/experiments/nib0004/initial_state.nc'
-fpath_tgrid  = 'auto'
-fpath_fx     = 'auto'
-
-oce_def = '_oce_def'
-oce_moc = '_oce_moc'
-oce_mon = '_oce_mon'
+# --- nc file prefixes
+oce_def     = '_oce_def'
+oce_moc     = '_oce_moc'
+oce_mon     = '_oce_mon'
+oce_ice     = '_oce_ice'
 oce_monthly = '_oce_dbg'
 
-atm_2d  = '_atm_2d_ml'
-atm_3d  = '_atm_3d_ml'
-atm_mon = '_atm_mon'
+atm_2d      = '_atm_2d_ml'
+atm_3d      = '_atm_3d_ml'
+atm_mon     = '_atm_mon'
 
+# --- time average information (can be overwritten by qp_driver call)
 tave_ints = [
-#['1480-02-01', '1490-01-01'],
-#['1580-02-01', '1590-01-01'],
 ['1630-02-01', '1640-01-01'],
-#['1780-02-01', '1790-01-01'],
 ]
