3260c59e17ef17d3ddf70a26c86d8c5f9911c181
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2020-08-07 13:36:57 +0200

    tools/run_qp_driver_r2b8.sh: Update to new structure.

--- tools/run_qp_driver_r2b8.sh
+++ tools/run_qp_driver_r2b8.sh
@@ -8,19 +8,64 @@
 #SBATCH --account=mh0033
 
 module list
-
 source /home/mpim/m300602/pyicon/tools/conda_act_mistral_pyicon_env.sh
 which python
 
-path_pyicon=`(cd .. && pwd)`"/"
-qp_driver="${path_pyicon}pyicon/quickplots/qp_driver.py"
-config_file="${path_pyicon}config_qp/conf-exp.ocean_omip_long_tke_r2b8_20134-WWG.py"
-path_qps="${path_pyicon}all_qps/"
+rand=$(cat /dev/urandom | tr -dc 'A-Z' | fold -w 3 | head -n 1)
 
+path_pyicon=`(cd .. && pwd)`"/"
+config_file="./config_qp_${rand}.py"
+qp_driver="${path_pyicon}pyicon/quickplots/qp_driver.py"
+
+cat > ${config_file} << %eof%
+# --- path to quickplots
+path_quickplots = '../all_qps/'
+
+# --- set this to True if the simulation is still running
+omit_last_file = True
+
+# --- do ocean and/or atmosphere plots
+do_atmosphere_plots = False
+do_ocean_plots      = True
+
+# --- grid information
+gname     = 'r2b8_oce_r0004'
+lev       = 'L128'
+
+# --- path to interpolation files
+path_grid        = '/mnt/lustre01/work/mh0033/m300602/icon/grids/'+gname+'/'
+path_ckdtree     = path_grid+'/ckdtree/'
+
+# --- grid files and reference data
+path_pool_oce       = '/pool/data/ICON/oes/input/r0004/OceanOnly_Global_IcosSymmetric_0010km_rotatedZ37d_modified_srtm30_1min/'
+gnameu = gname.split('_')[0].upper()
+fpath_tgrid         = path_pool_oce + gnameu+'_ocean-grid.nc'
+fpath_ref_data_oce  = path_pool_oce + gnameu+lev+'_initial_state.nc'
+fpath_fx            = path_pool_oce + gnameu+lev+'_fx.nc'
+
+# --- nc file prefixes
+oce_def     = ''
+oce_moc     = '_MOC'
+oce_mon     = '_oceanMonitor'
+oce_ice     = ''
+oce_monthly = ''
+
+# --- time average information (can be overwritten by qp_driver call)
+tave_ints = [
+['1630-02-01', '1640-01-01'],
+]
+%eof%
+
+# --- start qp_driver
 startdate=`date +%Y-%m-%d\ %H:%M:%S`
-# --- exp.ocean_omip_long_tke_r2b8_20134-WWG
-python -u ${qp_driver} --batch=True --path_quickplots=${path_qps} ${config_file} --run="exp.ocean_omip_long_tke_r2b8_20134-WWG" --tave_int='1932-02-01,1933-01-01'
+
+run="exp.ocean_ncep6h_r2b8_hel20160-NCP"
+path_data="/work/mh0033/m211054/projects/icon/icon-oes-1.3.01/experiments/${run}/outdata"
+python -u ${qp_driver} --batch=True ${config_file} --path_data=$path_data --run=$run --tave_int='1988-02-01,1989-01-01'
+
 enddate=`date +%Y-%m-%d\ %H:%M:%S`
+
+rm ${config_file}
 
 echo "--------------------------------------------------------------------------------"
 echo "Started at ${startdate}"
