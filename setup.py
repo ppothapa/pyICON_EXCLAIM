224b4874713e4f4109603f195132af86337f4287
Aaron Spring <aaron.spring@mpimet.mpg.de>
2021-08-20 14:22:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
 setup(
      name='pyicon',
      version='0.1.0',
@@ -12,15 +15,6 @@ setup(
      url='https://gitlab.dkrz.de/m300602/pyicon',
      author='Nils Brueggemann',
      author_email='nils.brueggemann@uni-hamburg.de',
-     install_requires=[
-        'numpy',
-        'scipy',
-        'xarray',
-        'cmocean',
-        'ipdb',
-        'netcdf4',
-        'matplotlib',
-        'cartopy'
-     ],
+     install_requires=install_requires
      setup_requires=['setuptools'],
 )
