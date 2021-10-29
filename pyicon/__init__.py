548dedb404b2cc68736d4ed92ce6d705433f00f5
Nils Brueggemann <nils.brueggemann@mpimet.mpg.de>
2021-10-29 13:32:53 +0200

    __init__.py: Including pyicon_calc_xr.py and some more verbose output.

--- pyicon/__init__.py
+++ pyicon/__init__.py
@@ -6,12 +6,20 @@ __email__ = "nils.brueggemann@uni-hamburg.de"
 __version__ = "0.1.0"
 
 # --- import pyicon basic modules
+print('-----calc')
 from .pyicon_calc import *
+print('-----calc_xr')
+from .pyicon_calc_xr import *
+print('-----tb')
 from .pyicon_tb import *
+print('-----IconData')
 from .pyicon_IconData import *
+print('-----plotting')
 from .pyicon_plotting import *
 
 # --- import pyicon.view
+print('-----view')
 from . import view
 # --- import pyicon.quickplots
+print('-----quickplots')
 from . import quickplots
