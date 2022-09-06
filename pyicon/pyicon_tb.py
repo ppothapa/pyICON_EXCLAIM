quickplots: Make correct time averaging possible for data where the time date data associated to the averaged data is within or at the beginning of the time averaging period (e.g. as it is the case for data post-processed with mkexp).

pyicon/pyicon_tb.py
@@ -820,6 +820,53 @@ def datetime64_to_float(dates):
   days   = int((str(dates.astype('datetime64[D]'))[8:10]))
   return years, months, days
 
+def get_averaging_interval(times, output_freq, end_of_interval=True):
+  """ Determine the length of the averaging time interval in seconds.
+
+  times: numpy array containing numpy.datetime64 objects: time points which are associated with averaing interval
+  output_freq: bool: pick either 'monthly', 'yearly', 'daily' or 'hourly'
+  end_of_interal: bool: sets whether time points of times are interpreted at the end of the averaging interval (default ICON output) or at the beginning or in the middle (mkexp time shift)
+  """
+  dt = np.zeros((times.size))
+  dt64type = times[0].dtype
+  if output_freq=='yearly':
+    for nn in range(times.size):
+      yy, mm, dd = datetime64_to_float(times[nn])
+      if end_of_interval:
+        y1, y2 = yy-1, yy
+      else:
+        y1, y2 = yy, yy+1
+      t1 = np.datetime64(f'{y1:04d}-01-01').astype(dt64type)
+      t2 = np.datetime64(f'{y2:04d}-01-01').astype(dt64type)
+      dt[nn] = t2-t1
+  elif output_freq=='monthly':
+    for nn in range(times.size):
+      yy, mm, dd = datetime64_to_float(times[nn])
+      if end_of_interval:
+        if mm==1:
+          m1, y1 = 12, yy-1
+          m2, y2 = 1,  yy
+        else:
+          m1, y1 = mm-1, yy
+          m2, y2 = mm, yy
+      else:
+        if mm==12:
+          m1, y1 = 12, yy
+          m2, y2 = 1,  yy+1
+        else:
+          m1, y1 = mm,   yy
+          m2, y2 = mm+1, yy
+      t1 = np.datetime64(f'{y1:04d}-{m1:02d}-01').astype(dt64type)
+      t2 = np.datetime64(f'{y2:04d}-{m2:02d}-01').astype(dt64type)
+      dt[nn] = t2-t1
+  elif output_freq=='daily':
+    dt += 86400.
+  elif output_freq=='hourly':
+    dt += 3600.
+  else:
+    raise ValueError(f'::: Error: Unsupported output_freq = {output_freq}!:::')
+  return dt
+
 def time_average(IcD, var, t1='none', t2='none', it_ave=[], iz='all', always_use_loop=False, verbose=False, use_xr=False, load_xr_data=False, dimension_from_file='first'):
   it_ave = np.array(it_ave)
   # --- if no it_ave is given use t1 and t2 to determine averaging indices it_ave
@@ -852,25 +899,27 @@ def time_average(IcD, var, t1='none', t2='none', it_ave=[], iz='all', always_use
   #else:
   #  ave_mode = 'unknown'
        
-  dt64type = IcD.times[0].dtype
-  time_bnds = IcD.times[it_ave]
-  yy, mm, dd = datetime64_to_float(time_bnds[0])
-  if t1!=t2:
-    if IcD.output_freq=='yearly':
-      time_bnds = np.concatenate(([np.datetime64(f'{yy-1:04d}-{mm:02d}-{dd:02d}').astype(dt64type)],time_bnds))
-    elif IcD.output_freq=='monthly':
-      if mm==1:
-        yy += -1
-        mm = 13
-      time_bnds = np.concatenate(([np.datetime64(f'{yy:04d}-{mm-1:02d}-{dd:02d}').astype(dt64type)],time_bnds))
-    elif IcD.output_freq=='unknown':
-      time_bnds = np.concatenate(([time_bnds[0]-(time_bnds[1]-time_bnds[0])], time_bnds))
-    dt = np.diff(time_bnds).astype(IcD.dtype)
-  else:
-    # load single time instance
-    dt = np.array([1])
-  #dt = np.ones((it_ave.size), dtype=IcD.dtype)
-  #print('Warning dt set to ones!!!')
+  # --- determine the length of the time average interval (in seconds)
+  #dt64type = IcD.times[0].dtype
+  #time_bnds = IcD.times[it_ave]
+  #yy, mm, dd = datetime64_to_float(time_bnds[0])
+  #if t1!=t2:
+  #  if IcD.output_freq=='yearly':
+  #    time_bnds = np.concatenate(([np.datetime64(f'{yy-1:04d}-{mm:02d}-{dd:02d}').astype(dt64type)],time_bnds))
+  #  elif IcD.output_freq=='monthly':
+  #    if mm==1:
+  #      yy += -1
+  #      mm = 13
+  #    time_bnds = np.concatenate(([np.datetime64(f'{yy:04d}-{mm-1:02d}-{dd:02d}').astype(dt64type)],time_bnds))
+  #  elif IcD.output_freq=='unknown':
+  #    time_bnds = np.concatenate(([time_bnds[0]-(time_bnds[1]-time_bnds[0])], time_bnds))
+  #  dt = np.diff(time_bnds).astype(IcD.dtype)
+  #else:
+  #  # load single time instance
+  #  dt = np.array([1])
+  ##dt = np.ones((it_ave.size), dtype=IcD.dtype)
+  ##print('Warning dt set to ones!!!')
+  dt = get_averaging_interval(IcD.times[it_ave], IcD.output_freq, end_of_interval=IcD.time_at_end_of_interval)
 
   # --- get dimensions to allocate data
   if dimension_from_file=='first':
@@ -1017,6 +1066,14 @@ def identify_grid(path_grid, fpath_data):
   """
   
   Dgrid_list = dict()
+
+  grid_name = 'r2b3_atm_r0030'; Dgrid_list[grid_name] = dict()
+  Dgrid_list[grid_name]['name'] = grid_name
+  Dgrid_list[grid_name]['res'] = '320km'
+  Dgrid_list[grid_name]['long_name'] = 'icon_grid_0030_R02B03_G'
+  Dgrid_list[grid_name]['size'] = 5120
+  #Dgrid_list[grid_name]['fpath_grid'] = path_grid + Dgrid_list[grid_name]['long_name'] + '/' + Dgrid_list[grid_name]['long_name'] + '.nc'
+  Dgrid_list[grid_name]['fpath_grid'] = f'{path_grid}/{grid_name}/{grid_name}_tgrid.nc'
 
   grid_name = 'r2b4_oce_r0003'; Dgrid_list[grid_name] = dict()
   Dgrid_list[grid_name]['name'] = grid_name
@@ -1321,7 +1378,6 @@ def get_timesteps(flist, time_mode='num2date'):
     ##  np_time = np.array(tstrs, dtype='datetime64')
     ##else:
     ##  raise ValueError('::: Error: Wrong time_mode %s in get_timesteps! :::' % time_mode)
-    #mybreak()
     #times[nn*nt:(nn+1)*nt] = np_time
     #flist_ts[nn*nt:(nn+1)*nt] = np.array([fpath]*nt)
     #its[nn*nt:(nn+1)*nt] = np.arange(nt)

pyicon/quickplots/qp_driver.py
@@ -70,8 +70,11 @@ time_mode_atm = 'num2date'  # 'num2date' is the new default previously, 'float2d
 tave_ints = [['1950-02-01', '1952-01-01']]
 # --- decide which data files to take for time series plots
 tstep     = '????????????????'
+tstep_ts  = '????????????????'
 # --- set this to 12 for yearly averages in timeseries plots, set to 0 for no averaging
 ave_freq = 12
+
+time_at_end_of_interval = True
 
 # --- xarray usage
 xr_chunks = None
@@ -268,6 +271,8 @@ tave_ints = {tave_ints}
 tstep     = \'{tstep}\'
 # --- set this to 12 for yearly averages in timeseries plots, set to 0 for no averaging
 ave_freq = {ave_freq}
+
+time_at_end_of_interval = {time_at_end_of_interval}
 
 # --- xarray usage
 xr_chunks = {xr_chunks}
@@ -553,6 +558,7 @@ if do_ocean_plots and not iopts.no_plots:
     calc_coeff             = False,
     load_xarray_dset       = load_xarray_dset,
     xr_chunks              = xr_chunks,
+    time_at_end_of_interval= time_at_end_of_interval,
     verbose                = verbose,
   )
 
@@ -577,6 +583,7 @@ if do_ocean_plots and not iopts.no_plots:
                  calc_coeff             = False,
                  load_xarray_dset       = load_xarray_dset,
                  xr_chunks              = xr_chunks,
+                 time_at_end_of_interval= time_at_end_of_interval,
                  verbose                = verbose, 
                 )
   fpath_ckdtree = IcD.rgrid_fpath_dict[rgrid_name]
@@ -584,7 +591,11 @@ if do_ocean_plots and not iopts.no_plots:
 
   DIcD = dict()
   for var in ['to', 'so', 'u', 'v', 'massflux', 'ice', 'monthly', 'mon', 'sqr']:
-    fname = '%s%s_%s.nc' % (run, D_variable_container[var], tstep)
+    if var=='mon':
+      tstep_tmp = tstep_ts
+    else:
+      tstep_tmp = tstep
+    fname = '%s%s_%s.nc' % (run, D_variable_container[var], tstep_tmp)
     print(f'Data container for {var}: {fname}')
     DIcD[var] = pyic.IconData(
       fname=fname, **IDsettings)
@@ -610,6 +621,7 @@ if do_ocean_plots and not iopts.no_plots:
                  load_triangular_grid   = False,
                  load_rectangular_grid  = True,
                  calc_coeff             = False,
+                 time_at_end_of_interval= time_at_end_of_interval,
                  verbose                = verbose,
                 )
   IcD_moc.depthc = IcD.depthc
@@ -700,6 +712,7 @@ if do_atmosphere_plots and not iopts.no_plots:
                  time_mode    = time_mode_atm,
                  model_type   = 'atm',
                  do_conf_dwd   = do_conf_dwd,
+                 time_at_end_of_interval= time_at_end_of_interval,
                 )
 
   fname_atm_3d = '%s%s_%s.nc' % (run, atm_3d, tstep)
@@ -723,10 +736,11 @@ if do_atmosphere_plots and not iopts.no_plots:
                  time_mode    = time_mode_atm,
                  model_type   = 'atm',
                  do_conf_dwd   = do_conf_dwd,
+                 time_at_end_of_interval= time_at_end_of_interval,
                 )
   fpath_ckdtree_atm = IcD_atm3d.rgrid_fpath_dict[rgrid_name_atm]
 
-  fname_atm_mon = '%s%s_%s.nc' % (run, atm_mon, tstep)
+  fname_atm_mon = '%s%s_%s.nc' % (run, atm_mon, tstep_ts)
   print('Dataset %s' % (fname_atm_mon))
   IcD_atm_mon = pyic.IconData(
                  fname        = fname_atm_mon,
@@ -747,6 +761,7 @@ if do_atmosphere_plots and not iopts.no_plots:
                  #time_mode    = 'float2date',
                  model_type   = 'atm',
                  do_conf_dwd   = do_conf_dwd,
+                 time_at_end_of_interval= time_at_end_of_interval,
                 )
   
   if do_ocean_plots==False:
@@ -773,6 +788,7 @@ if do_hamocc_plots and not iopts.no_plots:
                    load_triangular_grid   = True, # needed for bstr
                    load_rectangular_grid  = True,
                    calc_coeff             = False,
+                   time_at_end_of_interval= time_at_end_of_interval,
                    verbose                = verbose,
                   )
     fpath_ckdtree = IcD.rgrid_fpath_dict[rgrid_name]
@@ -795,6 +811,7 @@ if do_hamocc_plots and not iopts.no_plots:
                    load_triangular_grid   = False,
                    load_rectangular_grid  = True,
                    calc_coeff             = False,
+                   time_at_end_of_interval= time_at_end_of_interval,
                    verbose                = verbose,
                   )
     IcD_monthly.wet_c = IcD.wet_c
@@ -817,6 +834,7 @@ if do_hamocc_plots and not iopts.no_plots:
                  load_triangular_grid   = False,
                  load_rectangular_grid  = True,
                  calc_coeff             = False,
+                 time_at_end_of_interval= time_at_end_of_interval,
                  verbose                = verbose,
                 )
   IcD_ham_inv.wet_c = IcD.wet_c
@@ -838,11 +856,12 @@ if do_hamocc_plots and not iopts.no_plots:
                  load_triangular_grid   = False,
                  load_rectangular_grid  = True,
                  calc_coeff             = False,
+                 time_at_end_of_interval= time_at_end_of_interval,
                  verbose                = verbose,
                 )
   IcD_ham_2d.wet_c = IcD.wet_c
 
-  fname_ham_mon = '%s%s_%s.nc' % (run, ham_mon, tstep)
+  fname_ham_mon = '%s%s_%s.nc' % (run, ham_mon, tstep_ts)
   print('Dataset %s' % (fname_ham_mon))
   IcD_ham_mon = pyic.IconData(
                  fname        = fname_ham_mon,
@@ -858,6 +877,7 @@ if do_hamocc_plots and not iopts.no_plots:
                  load_triangular_grid   = False,
                  load_rectangular_grid  = False,
                  calc_coeff             = False,
+                 time_at_end_of_interval= time_at_end_of_interval,
                  verbose                = verbose,
                 )
 print('Done reading datasets')
@@ -964,8 +984,12 @@ for tave_int in tave_ints:
     it_ave_months = np.where( mask_int )[0]
     # Note: In ICON the date of an average is set to the end of the averaging interval
     #       Thus, the 4th month corresponds to March and the 10th to September
-    it_ave_mar = np.where( mask_int & (months==4)  )[0]
-    it_ave_sep = np.where( mask_int & (months==10) )[0]
+    if IcD_monthly.time_at_end_of_interval:
+      it_ave_mar = np.where( mask_int & (months==4)  )[0]
+      it_ave_sep = np.where( mask_int & (months==10) )[0]
+    else:
+      it_ave_mar = np.where( mask_int & (months==3) )[0]
+      it_ave_sep = np.where( mask_int & (months==9) )[0]
     if it_ave_mar.size==0:
       it_ave_mar = it_ave_months
     if it_ave_sep.size==0: